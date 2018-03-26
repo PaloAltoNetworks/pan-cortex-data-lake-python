68fe8a2f9dc81c24c62a72f952f5dafa0f294679
Steven Serrata <sserrata@paloaltonetworks.com>
2018-03-26 15:36:56 -0500

    Update __init__.py to allow package imports

--- pancloud/__init__.py
+++ pancloud/__init__.py
@@ -1,1 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""Main package for pancloud."""
+
+from .directorysync import DirectorySyncService
+from .event import EventService
+from .exceptions import PanCloudError, HTTPError, \
+    UnexpectedKwargsError, RequiredKwargsError
+from .httpclient import HTTPClient
+from .logging import LoggingService
+
+__all__ = [
+    'HTTPClient',
+    'DirectorySyncService',
+    'EventService',
+    'LoggingService',
+    'PanCloudError',
+    'HTTPError',
+    'UnexpectedKwargsError',
+    'RequiredKwargsError'
+]
+__author__ = 'Palo Alto Networks'
 __version__ = '1.0.2'
