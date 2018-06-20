9e7564f0156cc25cbb7b50fe9108528126f605c4
Steven Serrata <sserrata@paloaltonetworks.com>
2018-06-20 17:46:17 -0500

    Bump version: 1.2.1 → 1.2.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='pancloud',
-    version='1.2.1',
+    version='1.2.2',
     description="Python idiomatic SDK for the Palo Alto Networks Application Framework.",
     long_description=readme + '\n\n' + history,
     author="Steven Serrata",
