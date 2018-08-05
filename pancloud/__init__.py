cf5d238fca150b5309547b21e5e59c47d51a90f3
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2018-08-04 19:07:25 -0500

    Release 1.3.0 (#91)
    * Bump version: 1.2.3 → 1.3.0
    
    * Update HISTORY.rst

--- pancloud/__init__.py
+++ pancloud/__init__.py
@@ -3,7 +3,7 @@
 """Main package for pancloud."""
 
 __author__ = 'Palo Alto Networks'
-__version__ = '1.2.3'
+__version__ = '1.3.0'
 
 from .directorysync import DirectorySyncService
 from .event import EventService
