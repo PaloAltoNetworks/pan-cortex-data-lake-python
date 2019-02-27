8db330672dff4fab1286c19541c1738e68445fa1
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2019-02-27 17:55:25 -0600

    Release 1.5.0 (#127)
    * Release 1.5.0
    
    * Bump version: 1.4.0 → 1.5.0

--- pancloud/__init__.py
+++ pancloud/__init__.py
@@ -3,7 +3,7 @@
 """Main package for pancloud."""
 
 __author__ = 'Palo Alto Networks'
-__version__ = '1.4.0'
+__version__ = '1.5.0'
 
 from .directorysync import DirectorySyncService
 from .event import EventService
