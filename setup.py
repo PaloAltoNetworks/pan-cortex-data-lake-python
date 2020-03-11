4ec27ff9beb71fa52c6a2d5c01cb3e4ead3dabbd
Steven Serrata <sserrata@paloaltonetworks.com>
2020-03-11 18:21:31 -0500

    Use regex to compile list of required packages

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@
 """Palo Alto Networks Cloud Python SDK setup script."""
 
 from setuptools import setup, find_packages
+import re
 
 with open("README.md") as readme_file:
     readme = readme_file.read()
@@ -12,7 +13,10 @@ with open("HISTORY.md") as history_file:
     history = history_file.read()
 
 with open("requirements.txt") as requirements_file:
-    requirements = requirements_file.read().splitlines()
+    regex = re.compile(r"(.+==[0-9]+\.[0-9]+\.[0-9]+)")
+    requirements = list(
+        filter(None, [regex.findall(line) for line in requirements_file])
+    )
 
 setup_requirements = ["pytest-runner"]
 
