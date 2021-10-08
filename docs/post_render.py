97baa5c2432b7b1ac5fe7162237647e7ff4efc08
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2021-10-08 14:56:45 -0400

    replace .md with .py

--- docs/post_render.py
+++ docs/post_render.py
@@ -12,7 +12,7 @@ def main():
     md_files = glob.glob("./docs/**/*.md", recursive=True)
     for path in md_files:
         github_base_url = "https://github.com/PaloAltoNetworks/pan-cortex-data-lake-python/blob/master/pan_cortex_data_lake"
-        github_file_path = path.split("/reference")[1]
+        github_file_path = path.split("/reference")[1].replace(".md", ".py")
         custom_edit_url = f"{github_base_url}{github_file_path}"
         proc = EditFrontMatter(file_path=path, template_str=template_str)
         proc.run({"custom_edit_url": custom_edit_url})
