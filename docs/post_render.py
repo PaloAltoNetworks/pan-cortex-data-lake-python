fae1e380840f3952fec353f73becc9f7bed058b7
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2021-10-18 18:08:12 -0400

    Update post_render.py

--- docs/post_render.py
+++ docs/post_render.py
@@ -13,7 +13,7 @@ def main():
     for path in md_files:
         github_base_url = "https://github.com/PaloAltoNetworks/pan-cortex-data-lake-python/blob/master/pan_cortex_data_lake"
         github_file_path = path.split("/reference")[1].replace(".md", ".py")
-        custom_edit_url = f"{github_base_url}{github_file_path}"
+        custom_edit_url = f"{github_base_url}{github_file_path}"  # noqa: E999
         proc = EditFrontMatter(file_path=path, template_str=template_str)
         proc.run({"custom_edit_url": custom_edit_url})
         proc.writeFile(path)
